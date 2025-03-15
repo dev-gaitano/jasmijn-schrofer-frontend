aa7a339a0a38c09d69f66a97f4cbf152796f65b9
dev-gaitano <dev.gaitano@gmail.com>
2025-03-15 08:04:39 +0300

    Added styling to the pages in development

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,9 +1,20 @@
 const aboutPage = () => {
   return (
-    <div>
-      <h1>Page in Development</h1>
-      <p>Please come back later this page is still currently in development</p>
-      <a href="/">Go To Home page</a>
+    <div className="flex justify-center items-center h-screen ">
+      {/* Background Gradients */}
+      <div className="absolute inset-0 bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-primary/50 via-transparent to-transparent via-40% md:via-40% pointer-events-none"></div>
+      <div className="absolute inset-0 bg-[radial-gradient(ellipse_at_bottom_right,_var(--tw-gradient-stops))] from-[#008083]/50 via-transparent to-transparent via-40% pointer-events-none"></div>
+
+      {/* Content */}
+      <div className="flex flex-col gap-4 items-center ">
+        <h1 className="md:text-4xl font-bold">Page in Development</h1>
+        <p className="text-white/70">
+          Please come back later, this page is currently still in development
+        </p>
+        <a href="/" className="button-primary hover-lift mt-4">
+          Go To Home page
+        </a>
+      </div>
     </div>
   );
 };
