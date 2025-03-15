41cdcc098ea0477c06d4a4e8168185a81c336656
dev-gaitano <dev.gaitano@gmail.com>
2025-03-15 08:22:06 +0300

    Abstracted the development page into  a single page

--- src/pages/News.tsx
+++ src/pages/News.tsx
@@ -1,22 +1,7 @@
-const newsPage = () => {
-  return (
-    <div className="flex justify-center items-center h-screen ">
-      {/* Background Gradients */}
-      <div className="absolute inset-0 bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-primary/50 via-transparent to-transparent via-40% md:via-40% pointer-events-none"></div>
-      <div className="absolute inset-0 bg-[radial-gradient(ellipse_at_bottom_right,_var(--tw-gradient-stops))] from-[#008083]/50 via-transparent to-transparent via-40% pointer-events-none"></div>
+import InDevelopment from "./InDevelopment.tsx";
 
-      {/* Content */}
-      <div className="flex flex-col gap-4 items-center ">
-        <h1 className="md:text-4xl font-bold">Page in Development</h1>
-        <p className="text-white/70">
-          Please come back later, this page is currently still in development
-        </p>
-        <a href="/" className="button-primary hover-lift mt-4">
-          Go To Home page
-        </a>
-      </div>
-    </div>
-  );
+const newsPage = () => {
+  return <InDevelopment />;
 };
 
 export default newsPage;
