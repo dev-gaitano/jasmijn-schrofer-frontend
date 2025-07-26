d922db7715d5798d3d1a82ad6b7d8d9739214733
gaitano <dev.gaitano@gmail.com>
2025-07-26 17:45:08 +0300

    chore(dev): Switched from `lovable-tagger` to `swc-plugin-component-annotate`

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,26 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(() => ({
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" ? componentTagger() : null].filter(
-    Boolean
-  ),
+  plugins: [
+    react({
+      plugins: [
+        [
+          "swc-plugin-component-annotate",
+          {
+            "component-attr": "data-component",
+            "element-attr": "data-element",
+            "source-file-attr": "data-source-file",
+          },
+        ],
+      ],
+    }),
+  ],
   base: "./",
   build: {
     outDir: "dist",
