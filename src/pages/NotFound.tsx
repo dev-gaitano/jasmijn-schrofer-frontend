c4c70c1f48e3672d4855d6585940c9beed953d62
gaitano <dev.gaitano@gmail.com>
2025-07-31 09:47:26 +0300

    style(ui): added custom styling to not found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -12,12 +12,25 @@ const NotFound = () => {
   }, [location.pathname]);
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-800">
-      <div className="text-center">
-        <h1 className="text-4xl font-bold mb-4">404</h1>
-        <p className="text-xl text-gray-300 mb-4">Oops! Page not found</p>
-        <a href="/" className="text-blue-500 hover:text-blue-700 underline">
-          Return to Home
+    <div className="w-screen h-screen flex flex-col justify-center items-center overflow-hidden">
+      <h1 className="opacity-70 absolute blur-lg whitespace-nowrap text-[100vw] leading-none font-bold">
+        404
+      </h1>
+
+      {/* Background Gradients */}
+      <div className="absolute inset-0 bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-primary-more-muted via-transparent to-transparent via-40% md:via-40% pointer-events-none"></div>
+      <div className="absolute inset-0 bg-[radial-gradient(ellipse_at_bottom_right,_var(--tw-gradient-stops))] from-secondary-more-muted via-transparent to-transparent via-40% pointer-events-none"></div>
+
+      <div className="glass-panel flex flex-col justify-center items-center p-gap-md rounded-lg">
+        <p className="text-xl font-serif italic mb-gap-sm">
+          Oops! Page not found
+        </p>
+        <a
+          href="/"
+          className="inline-block button-primary hover-lift animate-fadeIn"
+          style={{ animationDelay: "1.4s" }}
+        >
+          Go to Home Page
         </a>
       </div>
     </div>
