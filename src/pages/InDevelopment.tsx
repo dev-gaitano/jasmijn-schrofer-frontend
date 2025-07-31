019626b60a2940a5025c3bdda03ed3980e4275da
gaitano <dev.gaitano@gmail.com>
2025-07-31 10:03:09 +0300

    refactor(variables/color): replaced inline colors with cutom css variables

--- src/pages/InDevelopment.tsx
+++ src/pages/InDevelopment.tsx
@@ -2,8 +2,8 @@ const inDevelopment = () => {
   return (
     <div className="flex justify-center items-center h-screen ">
       {/* Background Gradients */}
-      <div className="absolute inset-0 bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-primary/50 via-transparent to-transparent via-40% md:via-40% pointer-events-none"></div>
-      <div className="absolute inset-0 bg-[radial-gradient(ellipse_at_bottom_right,_var(--tw-gradient-stops))] from-[#008083]/50 via-transparent to-transparent via-40% pointer-events-none"></div>
+      <div className="absolute inset-0 bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-primary-more-muted via-transparent to-transparent via-40% md:via-40% pointer-events-none"></div>
+      <div className="absolute inset-0 bg-[radial-gradient(ellipse_at_bottom_right,_var(--tw-gradient-stops))] from-secondary-more-muted via-transparent to-transparent via-40% pointer-events-none"></div>
 
       {/* Content */}
       <div className="flex flex-col gap-4 items-center ">
