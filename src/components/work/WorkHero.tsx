d8b8ddc19067d3a3dccae9b84385dcbc58218ebc
gaitano <dev.gaitano@gmail.com>
2025-08-02 09:28:39 +0300

    feat(ui): integrated autoplay trailer

--- src/components/work/WorkHero.tsx
+++ src/components/work/WorkHero.tsx
@@ -1,29 +1,80 @@
-import { ChevronLeft, ChevronRight } from "lucide-react";
-import { Link } from "react-router-dom";
+import { AudioLines } from "lucide-react";
 import { WorkHeroProps } from "@/types/WorkHero";
 import BlurText from "@/components/BlurText";
 import { useIsOnScreen } from "@/hooks/useOnScreen";
+import { useRef, useEffect, useState } from "react";
 
 const WorkHero: React.FC<WorkHeroProps> = ({
   title,
   category,
   description,
   imagePath,
+  videoPath,
 }) => {
   const { isOnScreen } = useIsOnScreen();
+  const [isMuted, setIsMuted] = useState(true);
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    const video = videoRef.current;
+    if (video) {
+      video.muted = true;
+      console.log("mad");
+
+      setIsMuted(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    const video = videoRef.current;
+
+    if (!video) return;
+
+    if (isOnScreen) {
+      video.currentTime = 0;
+      video.play().catch(() => {}); // handle autoplay restrictions silently
+    } else {
+      video.pause();
+    }
+  }, [isOnScreen]);
+
+  const toggleMute = () => {
+    const video = videoRef.current;
+
+    if (video) {
+      video.muted = !video.muted;
+      setIsMuted(video.muted);
+
+      if (!video.muted) {
+        video.play().catch((err) => {
+          console.warn("Playback failed after unmuting", err);
+        });
+      }
+    }
+  };
 
   return (
     <section
       id="work-hero"
       className="relative p-gap-md md:p-gap-xxl h-mscreen md:h-screen w-full overflow-hidden"
     >
-      {/* Background Image with Overlay */}
-      <div
-        className="absolute inset-0 bg-cover bg-center bg-no-repeat"
-        style={{
-          backgroundImage: `url(${imagePath})`,
-        }}
-      ></div>
+      {/* Background */}
+      <div className="absolute inset-0 w-full h-full">
+        <video
+          ref={videoRef}
+          src={videoPath}
+          poster={imagePath}
+          preload="metadata"
+          loop
+          playsInline
+          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full object-cover transition-opacity duration-300"
+        />
+      </div>
+
+      {/* Desktop Gradients */}
+      <div className="md:hidden absolute inset-y-0 w-full bg-gradient-to-t from-background via-transparent to-transparent"></div>
+
+      {/* All Gradients */}
       <div className="absolute inset-0 md:bg-[radial-gradient(ellipse_at_bottom_right,_var(--tw-gradient-stops))] from-secondary-more-muted via-transparent to-transparent via-50% pointer-events-none"></div>
       <div className="absolute inset-0 bg-gradient-to-t md:bg-[radial-gradient(ellipse_at_bottom_left,_var(--tw-gradient-stops))] from-background-muted md:from-background-more-muted to-transparent via-20% md:via-50% pointer-events-none"></div>
 
@@ -57,7 +108,14 @@ const WorkHero: React.FC<WorkHeroProps> = ({
             className="inline-block button-primary hover-lift"
           >
             Watch Now
-          </Link>
+          </a>
+          {/* Audio Control */}
+          <button
+            className={`cursor-pointer ${isMuted ? "text-foreground-more-muted" : "text-primary-muted"} hover:text-primary-muted transition-all duration-500`}
+            onClick={toggleMute}
+          >
+            <AudioLines />
+          </button>
         </div>
       </div>
 
