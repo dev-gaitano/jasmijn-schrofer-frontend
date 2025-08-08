7e4b86c763dbb9bbfd5848b8e41cd3046f682593
gaitano <dev.gaitano@gmail.com>
2025-08-08 09:37:19 +0300

    style(ui): modified navbar to fit smaller screens and added active link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import { X } from "lucide-react";
 
+const currentPath = window.location.pathname;
+
 const MobileNavLink = ({
   href,
   children,
@@ -13,7 +15,8 @@ const MobileNavLink = ({
   <a
     href={href}
     onClick={onClick}
-    className="text-lg text-foreground hover:text-primary transition-colors duration-500 block py-gap-xxs"
+    className={`text-lg transition-colors duration-500 py-gap-xxs
+      ${currentPath === href ? "text-primary font-bold" : "text-foreground hover:text-primary"}`}
   >
     {children}
   </a>
@@ -33,115 +36,116 @@ const Navbar = () => {
 
   return (
     <nav
-      className={`fixed max-w-7xl max-h-[82px] top-0 z-[999] w-[78%] md:w-[1262px] flex items-center transition-all duration-500 ${
-        isScrolled ? "top-2 rounded-full glass-panel" : ""
+      className={`fixed max-w-7xl top-0 z-[999] w-[calc(100%-var(--gap-md)*2)] mx-gap-md md:w-[1262px] transition-all duration-500 overflow-hidden ${
+        isMobileMenuOpen
+          ? "rounded-3xl glass-panel top-2"
+          : isScrolled
+            ? "rounded-full glass-panel top-2"
+            : "rouded-3xl md:rounded-full"
       }`}
     >
-      <div className="container p-gap-xs">
-        <div className="flex items-center justify-between">
+      <div className="w-full flex items-center justify-between p-gap-xs">
+        <a
+          href="/"
+          className="text-2xl font-playfair font-bold text-gradient flex items-center ml-2"
+        >
+          <img
+            src="https://res.cloudinary.com/diwkfbsgv/image/upload/c_auto,f_auto,g_auto,h_40,q_auto/v1/schrofer/logo?_a=BAMAK+Go0"
+            alt="JAS Logo"
+            className="h-10"
+          />
+          {isScrolled || isMobileMenuOpen ? null : (
+            <p className="w-28 ml-1 text-base text-[var(--primary)]">
+              Jasmijn Schrofer
+            </p>
+          )}
+        </a>
+        <div className="h-12 hidden md:flex items-center space-x-8">
           <a
             href="/"
-            className="text-2xl font-playfair font-bold text-gradient flex items-center ml-2"
+            className={`nav-link hover-lift transition-all duration-500 
+              ${currentPath === "/" ? "text-primary font-bold" : ""}`}
           >
-            <img
-              src="https://res.cloudinary.com/diwkfbsgv/image/upload/c_auto,f_auto,g_auto,h_40,q_auto/v1/schrofer/logo?_a=BAMAK+Go0"
-              alt="JAS Logo"
-              className="h-10"
-            />
-            {isScrolled ? null : (
-              <p className="w-28 ml-1 text-base text-[var(--primary)]">
-                Jasmijn Schrofer
-              </p>
-            )}
+            Home
           </a>
-          <div className="h-12 hidden md:flex items-center space-x-8">
-            <a
-              href="/"
-              className="nav-link hover-lift transition-all duration-500"
-            >
-              Home
-            </a>
-            <a
-              href="/work"
-              className="nav-link hover-lift transition-all duration-500"
-            >
-              Work
-            </a>
-            <a
-              href="/about"
-              className="nav-link hover-lift transition-all duration-500"
-            >
-              About
-            </a>
-            {isScrolled ? (
-              <a
-                href="/contact"
-                className="inline-block button-primary hover-lift animate-fadeIn"
-                style={{ animationDelay: "0.6s" }}
-              >
-                Contact
-              </a>
-            ) : null}
-          </div>
-          <button
-            className="md:hidden mx-gap-xs text-primary"
-            onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+          <a
+            href="/work"
+            className={`nav-link hover-lift transition-all duration-500 ${
+              currentPath === "/work" ? "text-primary font-bold" : ""
+            }`}
           >
-            {isMobileMenuOpen ? (
-              <X className="w-8 h-8 mx-2" />
-            ) : (
-              <svg
-                className="w-8 h-8"
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path d="M4 6h16M4 12h16M4 18h16"></path>
-              </svg>
-            )}
-          </button>
-
-          {/* Mobile Menu */}
-          {isMobileMenuOpen && (
-            <div className="md:hidden absolute top-24 left-0 right-0 glass-panel rounded-lg">
-              {/*'top' attribute is 'nav' height + 'gap-md'*/}
-              <div className="flex flex-col items-center space-y-gap-xs p-gap-xs">
-                <MobileNavLink
-                  href="/"
-                  onClick={() => setIsMobileMenuOpen(false)}
-                >
-                  Home
-                </MobileNavLink>
-                <MobileNavLink
-                  href="/work"
-                  onClick={() => setIsMobileMenuOpen(false)}
-                >
-                  Work
-                </MobileNavLink>
-                <MobileNavLink
-                  href="/about"
-                  onClick={() => setIsMobileMenuOpen(false)}
-                >
-                  About
-                </MobileNavLink>
-                <MobileNavLink
-                  href="/contact"
-                  onClick={() => setIsMobileMenuOpen(false)}
-                >
-                  Contact
-                </MobileNavLink>
-                <MobileNavLink
-                  href="/admin"
-                  onClick={() => setIsMobileMenuOpen(false)}
-                >
-                  Admin
-                </MobileNavLink>
-              </div>
-            </div>
+            Work
+          </a>
+          <a
+            href="/about"
+            className={`nav-link hover-lift transition-all duration-500 ${
+              currentPath === "/about" ? "text-primary font-bold" : ""
+            }`}
+          >
+            About
+          </a>
+          {isScrolled ? (
+            <a
+              href="/contact"
+              className={`inline-block button-primary hover-lift animate-fadeIn ${
+                currentPath === "/contact" ? "text-primary font-bold" : ""
+              }`}
+              style={{ animationDelay: "0.6s" }}
+            >
+              Contact
+            </a>
+          ) : null}
+        </div>
+        <button
+          className="md:hidden mx-gap-xs text-primary"
+          onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+        >
+          {isMobileMenuOpen ? (
+            <X className="w-8 h-8" />
+          ) : (
+            <svg
+              className="w-8 h-8"
+              fill="none"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path d="M4 6h16M4 12h16M4 18h16"></path>
+            </svg>
           )}
+        </button>
+      </div>
+
+      {/* Mobile Menu */}
+      <div
+        className={`md:hidden transition-all duration-500 overflow-hidden ${
+          isMobileMenuOpen ? "max-h-96 opacity-100" : "max-h-0 opacity-0"
+        }`}
+      >
+        <div className="flex flex-col items-center p-gap-xs pt-0">
+          <MobileNavLink href="/" onClick={() => setIsMobileMenuOpen(false)}>
+            HOME
+          </MobileNavLink>
+          <MobileNavLink
+            href="/work"
+            onClick={() => setIsMobileMenuOpen(false)}
+          >
+            WORK
+          </MobileNavLink>
+          <MobileNavLink
+            href="/about"
+            onClick={() => setIsMobileMenuOpen(false)}
+          >
+            ABOUT
+          </MobileNavLink>
+          <MobileNavLink
+            href="/contact"
+            onClick={() => setIsMobileMenuOpen(false)}
+          >
+            CONATCT
+          </MobileNavLink>
         </div>
       </div>
     </nav>
